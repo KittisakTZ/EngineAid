a34e22829b4953a7df0aacf78d6527bb79b88330
KittisakTZ <Kittisak_m11@hotmail.com>
2025-04-16 18:27:01 +0700

    แก้ไขการตกเเต่งหน้า login,register

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,13 +1,19 @@
 // app/_layout.tsx
 import { Stack } from 'expo-router';
 import React from 'react';
+import { PaperProvider, DefaultTheme } from 'react-native-paper';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(auth)" />
-      <Stack.Screen name="(app)" />
-      <Stack.Screen name="index" />
-    </Stack>
+    // vvv ครอบด้วย PaperProvider vvv
+    <PaperProvider /* theme={theme} */ >
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(app)" />
+        <Stack.Screen name="index" />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </PaperProvider>
+    // ^^^ ครอบด้วย PaperProvider ^^^
   );
 }
